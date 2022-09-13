6f0cb0a38ff8eb867d20a4947122d1660a98dc34
RenStean <112504586+RenStean@users.noreply.github.com>
2022-09-13 15:00:14 -0600

    Add files via upload

---
+++ PerI/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    doble nota;
+    cout << "Ingrese la nota del examen de informatica" << endl;
+    cin >> nota;
+    cout << "nota del examen es: " << nota << endl;
+}
---
+++ PerI/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1, n2;
+    cout << "ingrese el primer numero: ";
+    cin >> n1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> n2;
+    if (n1 > n2){
+        cout << "El numero " <<n1 << "es el numero al numero " << n2 << endl;
+    
+    }
+}
